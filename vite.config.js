e7e3c23ba73974e579731afae02cd25aaee50ab3
Franco Emanuel Osman <francoosman12@gmail.com>
2025-04-17 10:56:45 -0500

    Viendo porque no funciona el deploy

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Dividir librerías grandes como React o Axios
-          vendor: ["react", "react-dom", "axios"]
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000 // Cambiar el límite de advertencia
-  }
-});
+})
